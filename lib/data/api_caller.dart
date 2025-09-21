e027dc6a786bff73665d4ba38cf9510a547fc95d
Mdarafath07 <mdarafathuddin10@gmail.com>
2025-09-22 01:15:17 +0600

    Add http and logger dependencies, implement ApiCaller for network requests

---
+++ lib/data/api_caller.dart
@@ -0,0 +1,92 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:logger/logger.dart';
+
+class ApiCaller {
+ static final Logger _logger = Logger();
+
+ static Future<ApiResponse> getRequest({required String url}) async {
+    Uri uri = Uri.parse(url);
+    _logRequest(url);
+    Response response = await get(uri);
+    _logRespose(url, response);
+    print(uri);
+    print(response.body);
+    print(response.statusCode);
+
+    final int statusCode = response.statusCode;
+
+    if (response.statusCode == 200) {
+      //success
+      final decodedData = jsonDecode(response.body);
+      return ApiResponse(
+        isSuccess: true,
+        responseCode: statusCode,
+        responseData: decodedData,
+      );
+    } else {
+      //faild
+      final decodedData = jsonDecode(response.body);
+      return ApiResponse(
+        isSuccess: false,
+        responseCode: statusCode,
+        responseData: decodedData,
+      );
+    }
+  }
+ static Future<ApiResponse> postRequest({required String url, Map<String,dynamic>? body}) async {
+    Uri uri = Uri.parse(url);
+    _logRequest(url, body: body);
+    Response response = await post(uri);
+    _logRespose(url, response);
+    print(uri);
+    print(response.body);
+    print(response.statusCode);
+
+    final int statusCode = response.statusCode;
+
+    if (statusCode == 200 || statusCode == 201) {
+      //success
+      final decodedData = jsonDecode(response.body);
+      return ApiResponse(
+        isSuccess: true,
+        responseCode: statusCode,
+        responseData: decodedData,
+      );
+    } else {
+      //faild
+      final decodedData = jsonDecode(response.body);
+      return ApiResponse(
+        isSuccess: false,
+        responseCode: statusCode,
+        responseData: decodedData,
+      );
+    }
+  }
+ static void _logRequest(String url, {Map<String, dynamic>? body}){
+    _logger.i("URL => $url\n"
+        "Request Body : $body"
+    );
+  }
+ static void _logRespose(String url, Response response){
+    _logger.i("URL => $url\n"
+        "Status Code : ${response.statusCode}\n"
+        "Body : ${response.body}"
+    );
+  }
+}
+
+class ApiResponse {
+  final bool isSuccess;
+  final int responseCode;
+  final dynamic responseData;
+  final String? errorMessage;
+
+  ApiResponse({
+    required this.isSuccess,
+    required this.responseCode,
+    required this.responseData,
+    this.errorMessage,
+  });
+}
